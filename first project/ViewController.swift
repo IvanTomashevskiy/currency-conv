24fc33ee01c4dd8949ee61cd4df8871682156bb9
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2020-05-31 17:12:18 +0300

    Initial Commit

---
+++ first project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first project
+//
+//  Created by Apple on 31.05.2020.
+//  Copyright © 2020 Ivan Tomashevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
